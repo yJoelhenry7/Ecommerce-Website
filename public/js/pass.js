8fc06ea10eec700ef444ebe42ba1b71160481ffa
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2022-10-03 12:57:09 +0530

    added js for pwd,forgot,verify ejs files

---
+++ public/js/pass.js
@@ -0,0 +1,45 @@
+const pwShowHide = document.querySelectorAll('.showHidePw');
+const pwdFields = document.querySelectorAll('.password');
+
+
+// function to checkpassword
+function checkPassword(){
+    let password = document.getElementById("password").value;
+    let cnfrmPassword = document.getElementById("cnfrmpassword").value;
+    console.log(password ,cnfrmPassword)
+    let messages = document.getElementById("message");
+    messages.style.display= "inline-block";
+    if(password.length != 0){
+        if(password == cnfrmPassword ){
+            messages.textContent = "Password Match";
+            messages.style.backgroundColor = "#3ae374";  
+        }
+        else{
+            messages.textContent = "Password don't match";
+            messages.style.backgroundColor = "#ff4d4d";
+        }
+    }
+}
+
+
+
+// js code to show/hide password and change icon
+pwShowHide.forEach(eyeIcon =>{
+    eyeIcon.addEventListener("click", ()=>{
+        pwdFields.forEach(pwField =>{
+            if(pwField.type ==="password"){
+                pwField.type = "text";
+
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye-slash","uil-eye");
+                })
+            }else{
+                pwField.type ="password";
+
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye","uil-eye-slash");
+                })
+            }
+        })
+    })
+})
